393b917a6e64e1831f32c098404b2937b9d162f5
TirkNork <91557636+TirkNork@users.noreply.github.com>
2024-04-28 02:55:55 +0700

    db integration demo

--- client/src/views/Test1/Test1.js
+++ client/src/views/Test1/Test1.js
@@ -1,13 +1,37 @@
 import { Link } from "react-router-dom";
+import Axios from "axios";
+import { useState, useEffect } from "react";
 
-function Test1(){
-  return(
+function Test1() {
+  const [testDataList, setTestDataList] = useState([]);
+
+  useEffect(() => {
+    getDataList();
+  }, []);
+
+  const getDataList = () => {
+    Axios.get("http://localhost:3001/testTable").then((response) => {
+      setTestDataList(response.data);
+    });
+  };
+
+  return (
     <div>
       <h1>Test1</h1>
       <button>
         <Link to="/">Go to HomeTest</Link>
       </button>
+      <h2>Test Data:</h2>
+      {testDataList.map((item) => (
+        <div key={item.id}>
+          <strong>ID:</strong> {item.id}, ;
+          <strong>First Name:</strong> {item.first_name}, ;
+          <strong>Last Name:</strong> {item.last_name}, ;
+          <strong>Age:</strong> {item.age}
+        </div>
+      ))}
     </div>
   );
 }
+
 export default Test1;
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors =require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "35.187.247.214",
+    password: "fra502test_password",
+    database: "fra502test",
+})
+
+app.get('/testTable', (req, res) => {
+    db.query("SELECT * FROM testTable", (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(result)
+        }
+    });
+});
+
+app.listen('3001', () => {
+    console.log('Server is running on port 3001')
+})
