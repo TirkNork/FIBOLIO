b0acdacd9a3249ea7c9b73a903d25d75c4517f3e
KRIneTic <thanakrit.msw@gmail.com>
2024-05-03 16:58:19 +0700

    test_dropdown

--- client/src/views/HomeTest/HomeTest.js
+++ client/src/views/HomeTest/HomeTest.js
@@ -3,9 +3,14 @@ function HomeTest() {
   return (
     <div>
       <h1>HomeTest</h1>
+      <div>
       <button>
         <Link to="/Test1">Go to Test1</Link>
       </button>
+      <button>
+        <Link to="/Course">Go to Course</Link>
+      </button>
+      </div>
     </div>
   );
 }
