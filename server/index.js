14a57fbff4c2bf95dcbebb6b888b57c19136ad1a
TirkNork <91557636+TirkNork@users.noreply.github.com>
2024-05-03 16:58:51 +0700

    update demo api with parameter

--- server/index.js
+++ server/index.js
@@ -1,29 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const cors =require('cors');
+const mysql = require("mysql");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user: "root",
-    host: "35.187.247.214",
-    password: "fra502test_password",
-    database: "fra502test",
-})
-
-app.get('/testTable', (req, res) => {
-    db.query("SELECT * FROM testTable", (err, result) => {
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send(result)
-        }
-    });
+  user: "root",
+  host: "35.187.247.214",
+  password: "fra502test_password",
+  database: "fra502test",
 });
 
-app.listen('3001', () => {
-    console.log('Server is running on port 3001')
-})
+// Ex. for basic query
+app.get("/testTable", (req, res) => {
+  db.query("SELECT * FROM testTable", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+// Ex. for query with parameter
+app.get("/testTable/:age", (req, res) => {
+  const age = req.params.age;
+  db.query("SELECT * FROM testTable WHERE age > ?", [age], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+
+app.listen("3001", () => {
+  console.log("Server is running on port 3001");
+});
