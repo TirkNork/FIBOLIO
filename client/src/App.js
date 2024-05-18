8d6763b629b3ad3471bade0188da7356fc04ebd7
TirkNork <91557636+TirkNork@users.noreply.github.com>
2024-05-18 15:33:03 +0700

    update front

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Test1 from './views/Test1/Test1.js';
 import HomeTest from './views/HomeTest/HomeTest.js';
-
+import Topbar from "./components/Topbar/Topbar.js";
 
 function App() {
   return (
     <BrowserRouter>
+    <Topbar/>
       <div className="App">
         {/* Route for all pages */}
         <Routes>
           <Route path="/" element={<div className="body"><HomeTest/></div>}></Route>
-          <Route path="/Test1" element={<div className="body"><Test1/></div>}></Route>
-          
-        </Routes>
-       
+          <Route path="/Test1" element={<div className="body"><Test1/></div>}></Route>    
+        </Routes>     
       </div>      
     </BrowserRouter>
   );
