6cdc19383df919a5c09ae7d83e78c5c4510f1a9f
pinhulk <pinhugtot@hotmail.com>
2024-05-21 00:15:17 +0700

    update student hash password done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
 const port = 3001;
+const bcrypt = require("bcrypt");
 
 app.use(cors());
 app.use(express.json());
@@ -24,21 +25,28 @@ const db = mysql.createConnection({
 //   });
 // });
 
-app.post("/Register01", (req, res) => {
+app.post("/Register01", async (req, res) => {
   console.log("Register01");
   const { studentID, name, surname, email, password } = req.body;
-  db.query(
-    "INSERT INTO StudentRegister (`studentID`, `name`, `surname`, `email`, `password`) VALUES (?, ?, ?, ?, ?)",
-    [studentID, name, surname, email, password],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(results);
-        console.log("Student Register Inserted");
+
+  try {
+    const hash = await bcrypt.hash(password, 12);
+    db.query(
+      "INSERT INTO StudentRegister (`studentID`, `name`, `surname`, `email`, `password`) VALUES (?, ?, ?, ?, ?)",
+      [studentID, name, surname, email, hash],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(results);
+          console.log("Student Register Inserted");
+        }
       }
-    }
-  );
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occurred while hashing the password.");
+  }
 });
 
 // app.post("/insertProjects", (req, res) => {
