26bb286ae5da43a8e763ebbac0c9c590e689b123
TirkNork <91557636+TirkNork@users.noreply.github.com>
2024-05-23 22:26:18 +0700

    update topbar

--- client/src/components/Topbar/Topbar.js
+++ client/src/components/Topbar/Topbar.js
@@ -1,17 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import './Topbar.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser, faCaretDown } from '@fortawesome/free-solid-svg-icons';
-import KMUTTlogo from '../../assets/images/KMUTT_Logo.png';
-import FIBOlogo from '../../assets/images/logo_FIBO_resized.png';
+import "./Topbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faCaretDown, faBars} from "@fortawesome/free-solid-svg-icons";
+import Fibolio from "../../assets/images/Fibolio.png";
 
 function Topbar() {
+
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
   const dropdownRef = useRef(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleResize = () => {
+    if (isMenuOpen && window.innerWidth > 580) {
+      setMenuOpen(!isMenuOpen);
+    }
+    if (dropdownOpen && window.innerWidth < 580) {
+      setDropdownOpen(!dropdownOpen);
+    }
   };
 
   const handleClickOutside = (event) => {
@@ -21,49 +35,100 @@ function Topbar() {
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+    document.removeEventListener("mousedown", handleClickOutside);
+    window.addEventListener("resize", handleResize);
+  });
+
+  useEffect(() => {
+    document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
   return (
-    <header>
-      <img src={KMUTTlogo} className="KMUTT_logo" alt="KMUTTlogo" />
-      <img src={FIBOlogo} className="FIBO_logo" alt="FIBOlogo" />
-      <div className="button-container">
-        <button className="btn-course">
-          <Link to="/PersonalInformation">Profile</Link>
-        </button>
-        <button className="btn-course">
-          <Link to="/Course">Course</Link>
-        </button>
-        <button className="btn-course">
-          <Link to="/Project">Project</Link>
-        </button>
-        <button className="btn-course">
-          <Link to="/Competency">Competency</Link>
-        </button>
+    <nav className="navbar">
+      <div className="left-element">
+        <img src={Fibolio} className="fibolio_logo" alt="Fiboliologo" />
+        <ul className="navbar-menu">
+          <li className="btn-course">
+            <Link to="/PersonalInformation">Profile</Link>
+          </li>
+
+          <li className="btn-course">
+            <Link to="/Course">Course</Link>
+          </li>
+
+          <li className="btn-course">
+            <Link to="/Project">Project</Link>
+          </li>
+
+          <li className="btn-course">
+            <Link to="/Competency">Competency</Link>
+          </li>
+        </ul>
       </div>
-      <nav>
-        <div className="dropdown-container" ref={dropdownRef}>
-          <div className="topbar-icon">
-            <div className="circleuser-icon">
-              <FontAwesomeIcon icon={faCircleUser} />
-            </div>
-            <div className="caretdown-icon" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={faCaretDown} />
-            </div>
+
+      <i className="user-button" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </i>
+
+      <div className="dropdown-menu">
+        <i>
+          <FontAwesomeIcon icon={faCircleUser} />
+        </i>
+        <i onClick={toggleDropdown}>
+          <FontAwesomeIcon icon={faCaretDown} />
+        </i>
+
+        {dropdownOpen && (
+          <div className="dropdown-content">
+            <Link to="#" onClick={() => setDropdownOpen(false)}>
+              User Name
+            </Link>
+            <Link to="/Signin" onClick={() => setDropdownOpen(false)}>
+              Logout
+            </Link>
           </div>
-          {dropdownOpen && (
-            <div className="dropdown-content">
-              <Link to="#" onClick={() => setDropdownOpen(false)}>Nuttida</Link>
-              <Link to="/Signin" onClick={() => setDropdownOpen(false)}>Logout</Link>
-            </div>
-          )}
-        </div>
-      </nav>
-    </header>
+        )}
+      </div>
+
+      {/* Menu for responsive Nav Bar*/}
+      {isMenuOpen ? (
+        <ul className="responsive-navbar">
+          <li>
+            <Link to="/PersonalInformation" onClick={toggleMenu}>
+              Profile
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/Course" onClick={toggleMenu}>
+              Course
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/Project" onClick={toggleMenu}>
+              Project
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/Competency" onClick={toggleMenu}>
+              Competency
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/Signin" onClick={toggleMenu}>
+              Logout
+            </Link>
+          </li>
+
+          <i className="user-close-button" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} />
+          </i>
+        </ul>
+      ) : null}
+    </nav>
   );
 }
 
