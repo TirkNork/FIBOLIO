f5b356e286e620b530adbf33360bebab58b26251
KRIneTic <thanakrit.msw@gmail.com>
2024-05-24 19:16:42 +0700

    90%

--- client/src/components/CourseDetailCard/CourseDetailCard.js
+++ client/src/components/CourseDetailCard/CourseDetailCard.js
@@ -5,16 +5,17 @@ function CourseDetailCard({ courseid, coursename, grade, academicYear, professor
     return (
         <div className="course-detail-card">
             <div className="course-detail-left">
-                <h2 className="course-detail-course-id">{courseid}</h2>
-                <p className="course-detail-course-name">{coursename}</p>
-                <p className="course-detail-class-type"><strong>Class Type:</strong> {classType}</p>
+            <div className="course-detail-header">
+    <h2 className="course-detail-header">{courseid} : {coursename}</h2>
+</div>
+                {/* <h2 className="course-detail-course-id">{courseid}</h2> */}
+                {/* <p className="course-detail-course-name">{coursename}</p> */}
+                <p className="course-detail-description"><strong>Description:</strong> {fullDescription}</p>
+                <p className="course-detail-class-type"><strong>Class:</strong> {classType}</p>
                 <p className="course-detail-professor"><strong>Professor:</strong> {professorname}</p>
                 <p className="course-detail-credits"><strong>Credits:</strong> {credits}</p>
                 <p className="course-detail-academic-year"><strong>Academic Year:</strong> {academicYear}</p>
-            </div>
-            <div className="course-detail-right">
                 <p className="course-detail-grade"><strong>Grade:</strong> {grade}</p>
-                <p className="course-detail-description"><strong>Description:</strong> {fullDescription}</p>
             </div>
         </div>
     );
