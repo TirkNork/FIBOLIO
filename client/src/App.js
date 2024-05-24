28fafec0961cdde8c216e22cf1a6f929ebc6d90a
TirkNork <91557636+TirkNork@users.noreply.github.com>
2024-05-24 19:51:13 +0700

    Revert "Merge pull request #12 from TirkNork/login-register"
    This reverts commit f808e6eb6248025c12b7c4a78a789de61796938a, reversing
    changes made to 9a9fe4726df1f2fbe0a5addf7f9297d7c2323182.

--- client/src/App.js
+++ client/src/App.js
@@ -12,18 +12,6 @@ import ProjectInsert from "./views/Project/ProjectInsert.js";
 import ProjectDetails from "./views/Project/ProjectDetails.js";
 import ProjectEdit from "./views/Project/ProjectEdit.js";
 import Topbar from "./components/Topbar/Topbar.js";
-import { BiHomeAlt } from "react-icons/bi";
-import Login from "./views//Login/Login";
-import Forgot from "./views//Login/Forgot";
-import Changepass from "./views//Login/Changepass";
-import Changepass_Teacher from "./views//Login/Changepass_Teacher";
-import Home from "./views//Login/Home";
-import Check from "./views//Login/Check";
-import Check_teacher from "./views//Login/Check_teacher";
-import TeacherPage from "./views//Login/TeacherPage";
-import RegisterTest from "./views/RegisterTest/RegisterTest.js";
-import Register from "./views/RegisterTest/Register01.jsx";
-
 
 function App() {
   return (
@@ -31,19 +19,8 @@ function App() {
     <Topbar/>
       <div className="App">
         {/* Route for all pages */}
-
         <Routes>
-          <Route path="/" element={<div className="body"><Login /></div>}></Route>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Forgot" element={<Forgot />} />
-          <Route path="/Changepass" element={<Changepass />} />
-          <Route path="/Changepass_Teacher" element={<Changepass_Teacher />} />
-          <Route path="/Check" element={<Check />} />
-          <Route path="/Check_teacher" element={<Check_teacher />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/TeacherPage" element={<TeacherPage />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/HomeTest" element={<div className="body"><HomeTest/></div>}></Route>
+          <Route path="/" element={<div className="body"><HomeTest/></div>}></Route>
           {/* <Route path="/Test1" element={<div className="body"><Test1/></div>}></Route>   */}
           <Route path="/Project" element={<div className="body"><Project/></div>}></Route>   
           <Route path="/ProjectInsert" element={<div className="body"><ProjectInsert/></div>}></Route>
